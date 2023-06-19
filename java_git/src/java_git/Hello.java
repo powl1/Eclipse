1aa07af12be039674ed61c44fc7280e4be75a10f
DESKTOP-K0NJGGT\USER <asm6396@naver.com>
2023-06-19 15:00:48 +0900

    커밋 한줄 추가.

--- java_git/src/java_git/Hello.java
+++ java_git/src/java_git/Hello.java
@@ -3,8 +3,9 @@ package java_git;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
 	}
 
 }
