fafcc3b970c9fa093608c12a8f38f45906f5b6e2
powl1 <135787778+powl1@users.noreply.github.com>
2023-08-17 17:39:08 +0900

    23.08.17

--- spring_boot_jyj_board/src/main/java/edu/global/ex/controller/HomeController.java
+++ spring_boot_jyj_board/src/main/java/edu/global/ex/controller/HomeController.java
@@ -1,13 +1,34 @@
 package edu.global.ex.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import lombok.extern.slf4j.Slf4j;
+
+// @RestController
+@Slf4j
+@Controller
 public class HomeController {
 	
 	@GetMapping("/")
 	public String home() {
-		return "<h1>안녕하세요</h1>";
+		return "home";
 	}
+	
+	@GetMapping("/user/userHome")
+	public void userHome() { // void : 적게 되면 url을 읽어버리게 된다.
+		log.info("userHome().. ");
+		// return "home";
+	}
+	
+	@GetMapping("/admin/adminHome")
+	public void adminHome() {
+		log.info("adminHome().. ");
+	}
+	
+//	@GetMapping("/user/userHome")
+//	public String userHome() {
+//		Log.info("userHome().. ");
+//		// return "home";
+//	}
 }
